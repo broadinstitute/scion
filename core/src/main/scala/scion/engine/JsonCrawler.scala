40226365983ca68feb3a6591f294779a2da99b36
Oliver Ruebenacker <oliverr@broadinstitute.org>
2019-02-15 12:07:24 -0500

    JSON crawler, first draft complete.

--- core/src/main/scala/scion/engine/JsonCrawler.scala
+++ core/src/main/scala/scion/engine/JsonCrawler.scala
@@ -1,24 +1,24 @@
 package scion.engine
 
 import io.circe.Json
-import scion.engine.JsonCrawler.{Analysis, ArrayMembership, Context, Membership, ObjectMembership}
+import scion.engine.JsonCrawler.{Analysis, Anchor, Context}
 
 class JsonCrawler[V](inspector: Context[V] => Analysis[V], recorder: Analysis[V] => Unit) {
 
-  def crawl(json: Json): Analysis[V] = crawl(json, None)
-
-  def crawl(json: Json, parent: Analysis[V], index: Long): Analysis[V] = crawl(json, Some(ArrayMembership(parent, index)))
-
-  def crawl(json: Json, parent: Analysis[V], key: String): Analysis[V] = crawl(json, Some(ObjectMembership(parent, key)))
-
-  def crawl(json: Json, parentOpt: Option[Membership[V]]): Analysis[V] = {
-    val context = Context(json, parentOpt)
+  def crawl(json: Json, anchor: Anchor[V] = Anchor.root): Analysis[V] = {
+    val context = Context(json, anchor)
     val analysis = inspector(context)
     recorder(analysis)
     if(json.isArray) {
-      ??? // TODO
+      for((child, index) <- json.asArray.get.zipWithIndex) {
+        val anchor = Anchor.inArray(analysis, index)
+        crawl(child, anchor)
+      }
     } else if(json.isObject) {
-      ??? // TODO
+      for((key, child) <- json.asObject.get.toList) {
+        val anchor = Anchor.inObject(analysis, key)
+        crawl(child, anchor)
+      }
     }
     analysis
   }
@@ -27,14 +27,29 @@ class JsonCrawler[V](inspector: Context[V] => Analysis[V], recorder: Analysis[V]
 
 object JsonCrawler {
 
-  trait Membership[V] {
+  sealed trait Anchor[+V] {
+    def parentOpt: Option[Analysis[V]]
+  }
+
+  object Anchor {
+    val root: Anchor[Nothing] = Root
+    def inArray[V](analysis: Analysis[V], index: Long): Anchor[V] = ArrayMembership(analysis, index)
+    def inObject[V](analysis: Analysis[V], key: String): Anchor[V] = ObjectMembership(analysis, key)
+  }
+
+  object Root extends Anchor[Nothing] {
+    override def parentOpt: None.type = None
+  }
+
+  sealed trait Membership[+V] extends Anchor[V] {
+    override def parentOpt: Some[Analysis[V]] = Some(parent)
     def parent: Analysis[V]
   }
 
-  case class ArrayMembership[V](parent: Analysis[V], index: Long) extends Membership[V]
-  case class ObjectMembership[V](parent: Analysis[V], key: String) extends Membership[V]
+  case class ArrayMembership[+V](parent: Analysis[V], index: Long) extends Membership[V]
+  case class ObjectMembership[+V](parent: Analysis[V], key: String) extends Membership[V]
 
-  case class Context[V](json: Json, parentOpt: Option[Membership[V]])
-  case class Analysis[V](value: V, context: Context[V])
+  case class Context[+V](json: Json, anchor: Anchor[V])
+  case class Analysis[+V](value: V, context: Context[V])
 
 }
